fd5538d852d70485552137963a6ef92d03ff62db
Евгений Васенев <vasenev.ea@mail.ru>
2018-10-07 22:10:37 +0300

    refactor

--- MultiSwitchSample/MultiSwitchSample/MultiToogleSwitch.xaml.cs
+++ MultiSwitchSample/MultiSwitchSample/MultiToogleSwitch.xaml.cs
@@ -12,21 +12,15 @@ namespace MultiSwitchSample
     public partial class MultiToogleSwitch : ContentView, INotifyPropertyChanged
     {
         public new Color BackgroundColor { get; set; } = Color.FromHex("#14B774");
-
         public List<ToogleCustomButton> ToogleButtons { get; set; } = new List<ToogleCustomButton>();
-
         public IToogled SelectedToogle { get; set; }
-
         public Color SelectedColor { get; set; } = Color.FromRgba(50, 249, 3, 137);
         public Color DefaultColor { get; set; } = Color.FromHex("#14B774");
-
         public Color SelectedBorderColor { get; set; } = Color.FromHex("#0CB90C");
         public Color DefaultBorderColor { get; set; } = Color.FromHex("#14B774");
 
         public float CornerRadius { get; set; } = 10;
-
         public double FontSize { get; set; }
-
         public string[] Toggles
         {
             get { return (string[])GetValue(TogglesProperty); }
@@ -49,19 +43,6 @@ namespace MultiSwitchSample
             }
         }
 
-
-
-        public string Test
-        {
-            get { return (string)GetValue(TestProperty); }
-            set { SetValue(TestProperty, 1); }
-        }
-
-        public static readonly BindableProperty TestProperty =
-    BindableProperty.Create("Test", typeof(string), typeof(MultiToogleSwitch),
-                "dsd",
-        BindingMode.TwoWay);
-
         public int SelectedId
         {
             get { return (int)GetValue(SelectedIdProperty); }
@@ -70,7 +51,7 @@ namespace MultiSwitchSample
 
         public static readonly BindableProperty SelectedIdProperty =
     BindableProperty.Create("SelectedId", typeof(int), typeof(MultiToogleSwitch),
-                0, BindingMode.TwoWay, propertyChanged: SelectedIdPropertyChanged);
+                default(int), BindingMode.TwoWay, propertyChanged: SelectedIdPropertyChanged);
 
         private static void SelectedIdPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
@@ -97,20 +78,18 @@ namespace MultiSwitchSample
             MainStack.Children.Clear();
             for (int i = 0; i < tooglesNames.Length; i++)
             {
-                var toogleButton = new ToogleCustomButton(i, CornerRadius, FontSize);
-                ToogleButtons.Add(toogleButton);
-                toogleButton.Command = ClickCommand;
+                var toogleButton = new ToogleCustomButton(i, CornerRadius, FontSize)
+                {
+                    Command = ClickCommand,
+                    SelectedColor = SelectedColor,
+                    SelectedBorderColor = SelectedBorderColor,
+                    DefaultColor = DefaultColor,
+                    DefaultBorderColor = DefaultBorderColor,
+                    Text = tooglesNames[i],
+                    CornerRadius = CornerRadius
+                };
                 toogleButton.CommandParameter = toogleButton;
-
-                toogleButton.SelectedColor = SelectedColor;
-                toogleButton.SelectedBorderColor = SelectedBorderColor;
-
-                toogleButton.DefaultColor = DefaultColor;
-                toogleButton.DefaultBorderColor = DefaultBorderColor;
-
-                toogleButton.Text = tooglesNames[i];
-                toogleButton.CornerRadius = CornerRadius;
-
+                ToogleButtons.Add(toogleButton);
                 MainStack.Children.Add(toogleButton);
                 MainStack.BackgroundColor = BackgroundColor;
             }
